6e8b9850c50435cf186fc54beb1f4d7375bc5fd9
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-21 14:40:08 +0100

    tests included

---
+++ test/system/games_test.rb
@@ -0,0 +1,10 @@
+require "application_system_test_case"
+
+class GamesTest < ApplicationSystemTestCase
+  test "Going to /new gives us a new random grid to play with" do
+    visit new_url
+    assert test: "New game"
+    assert_selector "li", count: 20
+    take_screenshot
+  end
+end
